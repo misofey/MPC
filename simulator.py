35731d6978df2432b0669f1eb4a57978b1954a14
Miso Fey <feymiso@gmail.com>
2025-04-04 23:43:49 +0200

    add ekf and move dynamics to different file

--- simulator.py
+++ simulator.py
@@ -24,6 +24,7 @@ from NLMPC import NLOcp
 from LMPC2 import LOcp
 from LPVMPC import LPVOcp
 
+from continuous_dynamics import Dynamics
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -33,65 +34,6 @@ from utils import path_planning
 from utils import plotting
 import logging
 
-
-class Dynamics:
-    def __init__(self, dt=0.002):
-        self.m = 180  # Car mass [kg]
-        self.I_z = 294  # TODO: unit
-        self.wbase = 1.53  # wheel base [m]
-        self.x_cg = 0.57  # C.G x location [m]
-        self.lf = self.x_cg * self.wbase  # Front moment arm [m]
-        self.lr = (1 - self.x_cg) * self.wbase  # Rear moment arm [m]
-
-        [self.Cf, self.Cr] = self.get_tyre_stiffness()
-        self.dt = dt
-
-    def get_tyre_stiffness(self) -> tuple[float, float]:
-        C_data_y = np.array(
-            [
-                1.537405752168591e04,
-                2.417765976460659e04,
-                3.121158998819641e04,
-                3.636055041362088e04,
-            ]
-        )
-        C_data_x = [300, 500, 700, 900]
-
-        Cf = np.interp((9.81 * self.m / 2) * (1 - self.x_cg), C_data_x, C_data_y)
-        Cr = np.interp((9.81 * self.m / 2) * self.x_cg, C_data_x, C_data_y)
-
-        return float(Cf), float(Cr)
-
-    def single_track_model(self, x, u) -> np.ndarray:
-        x_dot = np.zeros_like(x)
-        x_dot[0] = x[2] * x[4] - x[3] * x[5]  # px
-        x_dot[1] = x[3] * x[4] + x[2] * x[5]  # py
-        x_dot[2] = -x[6] * x[3]  # cos_head
-        x_dot[3] = x[6] * x[2]  # sin_head
-        x_dot[4] = 0  # vx
-        x_dot[5] = (
-            -(self.Cf + self.Cr) / (self.m * x[4]) * x[5]
-            + (-x[4] + (self.Cr * self.lr - self.Cf * self.lf) / (self.m * x[4])) * x[6]
-        ) - self.Cf / self.m * x[
-            7
-        ]  # vy
-        x_dot[6] = (
-            (self.lr * self.Cr - self.lf * self.Cf) / (self.I_z * x[4]) * x[5]
-            - (self.lf * self.lf * self.Cf + self.lr * self.lr * self.Cr)
-            / (self.I_z * x[4])
-            * x[6]
-            - (self.Cf * self.lf) / self.I_z * x[7]
-        )  # r
-        x_dot[7] = u
-        return x_dot
-
-    def rk4_integraton(self, xk, u) -> np.ndarray:
-        k1 = self.single_track_model(xk, u)
-        k2 = self.single_track_model(xk + self.dt / 2 * k1, u)
-        k3 = self.single_track_model(xk + self.dt / 2 * k2, u)
-        k4 = self.single_track_model(xk + self.dt * k3, u)
-
-        return xk + self.dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 class SkidpadSimulator:
     def __init__(
