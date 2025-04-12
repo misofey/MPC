51b7157f48a5bb5af8c9c9a5fc6c5695f888cf99
tzoli6 <zozoturi@gmail.com>
2025-04-12 13:31:36 +0200

    Correct mistakes in check function

--- stability.py
+++ stability.py
@@ -1,24 +1,28 @@
 import numpy as np
 
 def check_control_amissible_invariance(P, f:callable, K, c) -> bool:
-    n = P.shape[0]
-    eigvals, eigvecs = np.linalg.eigh(P)
-    Q = eigvecs  # columns are eigenvectors
     
+    # Obtain diagonal form of P
+    eigvals, eigvecs = np.linalg.eigh(P)
+    Q = eigvecs 
+    
+    # Bounds of outer axis parallel polyhadron in the derotated coordinate frame
     bounds = np.sqrt(c / eigvals)
 
+    # Generate vertices of the outer axis parallel polyhedron in the derotated coordinate frame
     vertices = np.array(np.meshgrid(*[[-b, b] for b in bounds])).T.reshape(-1, len(bounds))
     print(vertices.shape)
     print(2**(len(bounds)))
 
     for vertex in vertices:
-        # Transform to principal axes frame
-        x_next = f(vertex - K @ vertex)
-        print(x_next)
-        x_next_rot = Q @ x_next
-        print((Q @ vertex)-K @ (Q @ vertex))
-        # Check if the vertex is in the invariant set
-        if np.any(np.abs(x_next_rot) > bounds):
+        # Transform back to original coordinate frame
+        x = Q @ vertex
+        # Apply the control law
+        x_next = f(x - K @ x)
+        # Transform back to derotated coordinate frame
+        vertex_next = Q.T @ x_next
+        # Check if the vertex is in the proposed invariant set
+        if np.any(np.abs(vertex_next) > bounds):
             return False
         
     return True
